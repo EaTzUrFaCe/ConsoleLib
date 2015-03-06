6509146f78d8416a6ddea5e545ab892fec3b9a61
Matt Hermes <mathwhiz19@gmail.com>
2015-03-06 15:58:56 -0600

    Renamed Console, added basic line code.

---
+++ ConsoleLib/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleLib
+{
+    class Line:Widget
+    {
+        private enum Direction { HORIZONTAL, VERTICAL }
+
+
+
+        public Line(int x, int y, int width, int height, int layer = 0, bool hasBorder = false)
+            : base(x, y, width, height, layer, hasBorder)
+        {
+            if ((height == 1 && width == 1)||(height != 1 && width != 1))
+            {
+
+            }
+        }
+
+        public Line(Coord topLeftCorner, Coord bottomRightCorner, int layer = 0, bool hasBorder = false)
+            : base(topLeftCorner, bottomRightCorner, layer, hasBorder)
+        {
+        }
+
+        public override void Draw()
+        {
+
+        }
+    }
+}
